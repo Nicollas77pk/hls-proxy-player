81fa968a015035ce84a4b05c2af6d7a31256cfdb
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-20 17:17:15 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,17 +1,8 @@
-export default async function handler(req, res) {
-  const url = req.query.url;
-  if (!url || !url.startsWith("http://")) {
-    return res.status(400).send("URL inválida ou não permitida.");
-  }
-
-  try {
-    const response = await fetch(url);
-    const contentType = response.headers.get("content-type");
-    const body = await response.arrayBuffer();
-    res.setHeader("Content-Type", contentType);
-    res.send(Buffer.from(body));
-  } catch (error) {
-    res.status(500).send("Erro ao buscar o conteúdo.");
-  }
-}
-
+module.exports = async (req, res) => {
+  const targetUrl = req.query.url; // URL m3u8 fornecida como query string
+  const response = await fetch(targetUrl);
+  const data = await response.text();
+  
+  res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+  res.status(200).send(data);
+};
