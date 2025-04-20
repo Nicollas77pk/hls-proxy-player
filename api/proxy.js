b1b6b60a14e38683cbe351d6a798fde85fbb9d31
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-20 16:51:15 -0300

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,17 @@
-g
+export default async function handler(req, res) {
+  const url = req.query.url;
+  if (!url || !url.startsWith("http://")) {
+    return res.status(400).send("URL inválida ou não permitida.");
+  }
+
+  try {
+    const response = await fetch(url);
+    const contentType = response.headers.get("content-type");
+    const body = await response.arrayBuffer();
+    res.setHeader("Content-Type", contentType);
+    res.send(Buffer.from(body));
+  } catch (error) {
+    res.status(500).send("Erro ao buscar o conteúdo.");
+  }
+}
+
