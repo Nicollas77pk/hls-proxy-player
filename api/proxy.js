37f132df4b697ac6855b367ef1a3602a821b7de4
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-22 08:50:03 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,16 @@
 module.exports = async (req, res) => {
-  const targetUrl = req.query.url; // URL m3u8 fornecida como query string
-  const response = await fetch(targetUrl);
-  const data = await response.text();
-  
-  res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
-  res.status(200).send(data);
+  const targetUrl = req.query.url;
+  if (!targetUrl) return res.status(400).send("URL não fornecida.");
+
+  try {
+    const response = await fetch(targetUrl);
+    const contentType = response.headers.get("content-type") || 'application/vnd.apple.mpegurl';
+    const data = await response.text();
+
+    res.setHeader("Content-Type", contentType);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send("Erro ao buscar o stream.");
+  }
 };
