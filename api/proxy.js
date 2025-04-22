a3a4b775e376341d8d6aa6afd93d3ce767531efb
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-22 09:40:23 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,19 @@
+const fetch = require('node-fetch');
+
 module.exports = async (req, res) => {
   const targetUrl = req.query.url;
   if (!targetUrl) return res.status(400).send("URL não fornecida.");
 
   try {
     const response = await fetch(targetUrl);
-    const contentType = response.headers.get("content-type") || 'application/vnd.apple.mpegurl';
-    const data = await response.text();
+    const contentType = response.headers.get("content-type") || "application/vnd.apple.mpegurl";
+    const buffer = await response.buffer(); // Usa buffer ao invés de text()
 
     res.setHeader("Content-Type", contentType);
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(data);
+    res.status(200).send(buffer); // Retorna como buffer
   } catch (err) {
+    console.error("Erro ao buscar o stream:", err);
     res.status(500).send("Erro ao buscar o stream.");
   }
 };
