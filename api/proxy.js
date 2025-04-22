23cc1f4d7087e614e76c4e27ba5bddcfceb80a7a
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-22 08:54:02 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,24 +1,8 @@
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
-  const { url } = req.query;
+  const url = req.query.url;
   if (!url) return res.status(400).send("URL não fornecida.");
 
-  try {
-    const response = await fetch(url, {
-      headers: {
-        "User-Agent": req.headers['user-agent'] || '',
-        "Referer": req.headers['referer'] || '',
-      }
-    });
-
-    const contentType = response.headers.get("content-type") || 'application/octet-stream';
-    res.setHeader("Content-Type", contentType);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    const body = await response.text();
-    res.status(200).send(body);
-  } catch (error) {
-    res.status(500).send("Erro ao obter conteúdo.");
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.writeHead(302, { Location: url });
+  res.end();
 };
