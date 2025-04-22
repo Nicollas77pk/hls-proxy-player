9d28c82c74807b4f8872f8601e60ae3ed0bf1143
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-22 09:21:01 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,8 @@
-const https = require('https');
-const http = require('http');
-
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).send("URL não fornecida.");
 
-  const client = url.startsWith('https') ? https : http;
-
-  client.get(url, (streamRes) => {
-    res.setHeader('Content-Type', streamRes.headers['content-type'] || 'application/vnd.apple.mpegurl');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Cache-Control", "no-cache");
-    streamRes.pipe(res);
-  }).on('error', () => {
-    res.status(500).send("Erro ao redirecionar o stream.");
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.writeHead(302, { Location: url });
+  res.end();
 };
