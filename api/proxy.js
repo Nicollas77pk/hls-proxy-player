be6411c832f9c9add77329cb84d29018b4b55941
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-22 08:59:15 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,20 +1,8 @@
-const fetch = require("node-fetch");
-
 module.exports = async (req, res) => {
-  const { url } = req.query;
+  const url = req.query.url;
   if (!url) return res.status(400).send("URL não fornecida.");
 
-  try {
-    const response = await fetch(url);
-    const m3u8 = await response.text();
-
-    const baseUrl = url.split("/").slice(0, -1).join("/");
-    const proxied = m3u8.replace(/(.*\.ts)/g, match => `${req.url.split("?")[0]}?url=${baseUrl}/${match}`);
-
-    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(proxied);
-  } catch (err) {
-    res.status(500).send("Erro ao carregar playlist.");
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.writeHead(302, { Location: url });
+  res.end();
 };
